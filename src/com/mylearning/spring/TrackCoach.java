b6602cb5489dedfe88156893ac25e393e1e544e2
dsrathore344 <rathore344@gmail.com>
2019-05-26 11:03:27 +0530

    Adding Spring BeanLifeCycle files

--- src/com/mylearning/spring/TrackCoach.java
+++ src/com/mylearning/spring/TrackCoach.java
@@ -20,5 +20,12 @@ public class TrackCoach implements Coach {
 		
 		return "Just do it:"+ fortuneService.getFortune();
 	}
-
+	//define init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff ");
+	}
+	//define destroy method
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuff ");
+	}
 }
